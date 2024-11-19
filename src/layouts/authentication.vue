e4c90399fdbc53b124c8dbf2e2a8f827f0da590b
daddy-chill <blacklistuser420@gmail.com>
2024-11-19 19:41:15 +0630

    update test

---
+++ src/layouts/authentication.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  const isLogin = ref<boolean>(false);
+  const image = isLogin ? 'red':'blue';
+  const styleBackground = computed(() => ({
+  // backgroundImage: `url(${backgroundImageUrl.value})`,
+  backgound:image,
+  // backgroundSize: 'cover',
+  // backgroundPosition: 'center'
+}));
+
+console.log(styleBackground)
+</script>
+
+<template>
+  <div class="flex justify-center items-center min-h-screen" :style="styleBackground.value">
+    <slot />
+  </div>
+</template>
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+definePageMeta({ layout: "authentication" });
 import type { LoginPayload } from "../types/auth";
 import { reactive, ref } from "vue";
 import { useAuthStore } from "../stores/auth";
@@ -6,6 +7,8 @@ import type { VForm } from "vuetify/components";
 
 const store = useAuthStore();
 const form = ref<VForm>();
+
+
 
 
 /**
@@ -27,36 +30,45 @@ const login = async () => {
 };
 </script>
 <template>
-  <v-container
-    class="flex justify-center items-center min-h-screen bg-gray-100"
-  >
-    <v-card class="w-full max-w-md p-6">
-      <v-card-title class="text-center text-lg font-bold">Login</v-card-title>
-      <v-card-text>
-        <v-form ref="form" @submit.prevent="login">
-          <v-text-field
-            v-model="loginPayload.email"
-            label="Email"
-            type="email"
-            prepend-icon="mdi-email"
-            required
-          ></v-text-field>
-          <v-text-field
-            v-model="loginPayload.password"
-            label="Password"
-            type="password"
-            prepend-icon="mdi-lock"
-            required
-          ></v-text-field>
-          <v-btn class="w-full mt-4" color="primary" type="submit">
-            Login
-          </v-btn>
-        </v-form>
-      </v-card-text>
-    </v-card>
-  </v-container>
+  <v-card class="w-full max-w-md p-6">
+    <v-card-title>Login</v-card-title>
+  </v-card>
 </template>
 
 <style scoped>
 /* Additional styles can be added here if needed */
 </style>
+
+
+
+
+
+
+<!-- <v-container
+class="flex justify-center items-center min-h-screen bg-gray-100"
+>
+<v-card class="w-full max-w-md p-6">
+  <v-card-title class="text-center text-lg font-bold">Login</v-card-title>
+  <v-card-text>
+    <v-form ref="form" @submit.prevent="login">
+      <v-text-field
+        v-model="loginPayload.email"
+        label="Email"
+        type="email"
+        prepend-icon="mdi-email"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="loginPayload.password"
+        label="Password"
+        type="password"
+        prepend-icon="mdi-lock"
+        required
+      ></v-text-field>
+      <v-btn class="w-full mt-4" color="primary" type="submit">
+        Login
+      </v-btn>
+    </v-form>
+  </v-card-text>
+</v-card>
+</v-container> -->
