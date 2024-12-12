f6147fcd4d673910cfbd6a11a2e16ec796846914
ayenadikyaw <dev.exbrain.phway@gmail.com>
2024-12-12 16:44:51 +0630

    Add Summary Page

---
+++ src/components/order/ProductSummary.vue
@@ -0,0 +1,113 @@
+<script setup>
+import productList from "~/pages/order/productList.json";
+import productCategory from "~/pages/product/productcategory.json";
+import goldType from "~/pages/product/goldtype.json";
+import productType from "~/pages/product/producttype.json";
+import { useOrderStore } from "~/stores/order";
+
+//data
+const products = useOrderStore().products;
+const productCategories = ref(productCategory);
+const goldTypes = ref(goldType);
+const productTypes = ref(productType);
+</script>
+<template>
+  <div class="text-sm">
+    <div>
+      <!--table-->
+      <div class="w-full">
+        <div class="overflow-x-auto">
+          <DataTable
+            :value="products"
+            stripedRows
+            class="w-full text-sm"
+            scrollable
+            resizableColumns
+            columnResizeMode="expand"
+            showGridlines
+            paginator
+            :rows="14"
+            :rowsPerPageOptions="[10, 20, 50]"
+            :totalRecords="products.length"
+            responsiveLayout="scroll"
+            breakpoint="sm"
+          >
+            <!--Image-->
+            <Column field="image" class="w-[10%]">
+              <template #body="slotProps">
+                <div>
+                  <img
+                    :src="slotProps.data.image"
+                    alt="product image"
+                    class="w-12 h-12 object-cover"
+                  />
+                </div>
+              </template>
+            </Column>
+            <!--Product Name-->
+            <Column field="name" class="w-[15%]">
+              <template #body="slotProps">
+                <div class="font-bold">Product Name</div>
+                <div>
+                  {{ slotProps.data.code }}
+                </div>
+              </template>
+            </Column>
+            <!--Category-->
+            <Column field="category" header="Category" class="w-[15%]">
+              <template #body="slotProps">
+                <div>
+                  {{
+                    productCategories.find(
+                      (category) => category.id === slotProps.data.category_id
+                    ).value
+                  }}
+                </div>
+              </template>
+            </Column>
+            <!--Type-->
+            <Column field="type" header="Type" class="w-[15%]">
+              <template #body="slotProps">
+                <div>
+                  {{
+                    productTypes.find(
+                      (type) => type.id === slotProps.data.type_id
+                    ).value
+                  }}
+                </div>
+              </template>
+            </Column>
+            <!--Size-->
+            <Column field="size" header="Size" class="w-[15%]">
+              <template #body="slotProps">
+                <div>
+                  {{ slotProps.data.size }}
+                </div>
+              </template>
+            </Column>
+            <!--Quantity-->
+            <Column field="available_stock" header="Available" class="w-[15%]">
+              <template #body="slotProps">
+                <div>
+                  {{ slotProps.data.available_stock }}
+                </div>
+              </template>
+            </Column>
+            <!--Gold Type-->
+            <Column field="goldType" header="Gold Type" class="w-[15%]">
+              <template #body="slotProps">
+                <div>
+                  {{
+                    goldTypes.find(
+                      (goldType) => goldType.id === slotProps.data.gold_type_id
+                    ).value
+                  }}
+                </div>
+              </template>
+            </Column>
+          </DataTable>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/pages/order/summary.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+import tracking from "@/components/order/tracking.vue";
+import paymentForm from "@/components/order/paymentForm.vue";
+import invoice from "@/components/order/invoice.vue";
+import customerInfo from "@/components/order/customerInfo.vue";
+import shippingInfo from "~/components/order/shippingInfo.vue";
+import { useOrderStore } from "@/stores/order";
+import { computed, ref } from "vue";
+import ProductSummary from "~/components/order/ProductSummary.vue";
+import CommentSection from "~/components/order/commentSection.vue";
+//import type { Customer } from "@/types/customer";
+
+//constants
+const orderStore = useOrderStore();
+const payment = ref({});
+
+//functions
+/**
+ * Fill payment data to the form
+ * @param payment
+ */
+const handleAddPayment = (payment: any) => {
+  console.log("add new payment", payment);
+  payment.value = payment;
+};
+
+const printOrder = () => {
+  console.log("print order");
+};
+
+const finishedOrder = () => {
+  console.log("finished order");
+};
+</script>
+
+<template>
+  <div class="min-h-screen mb-8">
+    <tracking />
+    <div class="px-3 py-6 flex justify-center gap-10">
+      <div class="w-3/4 flex flex-col gap-4">
+        <div class=" bg-accentwhite py-6 px-3 drop-shadow-md rounded-lg">
+          <ProductSummary />
+        </div>
+        <div class=" bg-accentwhite py-6 px-3 drop-shadow-md rounded-lg">
+          <CommentSection />
+        </div>
+      </div>
+
+      <div class="w-1/3 flex flex-col gap-4">
+        <div class="bg-accentwhite py-6 px-3 drop-shadow-md rounded-lg">
+          <invoice />
+        </div>
+        <div
+          class="bg-accentwhite py-6 px-3 drop-shadow-md rounded-lg overflow-x-auto"
+        >
+          <customerInfo />
+        </div>
+        <div
+          class="bg-accentwhite py-6 px-3 drop-shadow-md rounded-lg overflow-x-auto"
+        >
+          <shippingInfo />
+        </div>
+      </div>
+    </div>
+
+    <div class="mb-8 flex justify-between">
+      <Button @click="navigateTo('/order/shipping')" class="underline"
+        >Back</Button
+      >
+      <div class="flex gap-4">
+      <button
+        @click="printOrder"
+        class="bg-primarylight text-white px-6 py-1 rounded-md float-right mb-8"
+      >
+        <span class="text-white">Print</span>
+      </button>
+      <button
+        @click="finishedOrder"
+        class="bg-primarylight text-white px-6 py-1 rounded-md float-right mb-8"
+      >
+        <span class="text-white">Finished</span>
+      </button>
+      </div>
+    </div>
+  </div>
+</template>
